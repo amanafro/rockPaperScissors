3a812bfb5dd986178d9f8ae2de113bd48fcc8c62
aman <amanuelisaak15@gmail.com>
2023-11-10 10:06:21 +0100

    commit nr. 4

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const ROCK = "rock✊";
-const PAPER = "paper🤚";
-const SCISSORS = "scissors✌";
+//Defining Constants
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
 
+//Linking to the HTLM
 const rockbtn = document.getElementById("rock-button");
 const paperbtn = document.getElementById("paper-button");
 const scissorsbtn = document.getElementById("scissors-button");
@@ -10,43 +12,51 @@ const restbtn = document.getElementById("reset")
 const resultMessage = document.getElementById("result-message");
 const scoreMessage = document.getElementById("score-message")
 
+
 rockbtn.addEventListener("click", () => playerMove(ROCK));
 paperbtn.addEventListener("click", () => playerMove(PAPER));
 scissorsbtn.addEventListener("click", () => playerMove(SCISSORS));
 
-
-
-function playerMove(playerMove) {
-  const bot = botMove();
-
-  let result = "";
-
-  if (bot === playerMove) {
-    result = "It's a tie 😑";
-  } else if (
-    (bot === PAPER && playerMove === ROCK) ||
-    (bot === SCISSORS && playerMove === PAPER) ||
-    (bot === ROCK && playerMove === SCISSORS)
-  ) {
-    result = "You lose 🤡";
-  } else {
-    result = "You win 🥳";
-  }
-
-  resultMessage.textContent = `You picked ${playerMove}. The bot picked ${bot}, so ${result}`;
-
+//This will be the score store
   const score = {
   wins: 0,
   loses: 0,
   draws: 0
 };
 
+//This is the constant which will store the user input result
+const playerResult = playerInputResult()
+  
+function playerInputResult(input) {
+  return input
+}
 
-  if (result === "You win 🥳") {
+//This is what the player will input
+function playerMove(playerMove) {
+  const bot = botMove();
+
+  let result = "";
+
+  if (bot === playerMove) {
+    result = "It's a tie";
+  } else if (
+    (bot === PAPER && playerMove === ROCK) ||
+    (bot === SCISSORS && playerMove === PAPER) ||
+    (bot === ROCK && playerMove === SCISSORS)
+  ) {
+    result = "You lose";
+  } else {
+    result = "You win";
+  }
+
+  //TODO: -Do some conditional rendering to show the icons of the bot move 
+  resultMessage.textContent = `You picked ${playerMove}. The bot picked ${bot}, so ${result}`;
+
+  if (result === "You win") {
       score.wins += 1;
-  } else if (result === "You lose 🤡") {
+  } else if (result === "You lose") {
       score.losses += 1;
-  } else if (result === "It's a tie 😑") {
+  } else if (result === "It's a tie") {
       score.ties += 1;
   }
 
@@ -56,18 +66,18 @@ function playerMove(playerMove) {
   
 }
 
+//This function will generate the bot result and return it
 function botMove() {
   const randomNumber = Math.random();
 
   let botMove = "";
 
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
-    botMove = "rock✊";
+    botMove = "rock";
   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    botMove = "paper🤚";
+    botMove = "paper";
   } else {
-    botMove = "scissors✌";
+    botMove = "scissors";
   }
-
   return botMove;
 }
